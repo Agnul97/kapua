bfd1af90f9caa73d59087cee3e686229690860fc
angelo.andreussi <angelo.andreussi@eurotech.com>
2023-07-27 11:20:06 +0200

    xzcxzcx

--- qa/integration/src/test/java/org/eclipse/kapua/integration/service/device/RunDeviceDataI9nTest.java
+++ qa/integration/src/test/java/org/eclipse/kapua/integration/service/device/RunDeviceDataI9nTest.java
@@ -16,7 +16,7 @@ package org.eclipse.kapua.integration.service.device;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith; //TODO: TEST
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
