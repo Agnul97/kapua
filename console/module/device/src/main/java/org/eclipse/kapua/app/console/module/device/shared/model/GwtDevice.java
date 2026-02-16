1b4a444c0bcd1c48c060161aa33bcbf6c93e6e77
angelo.andreussi <angelo.andreussi@gmail.com>
2026-02-16 16:07:07 +0100

    :sparkles: added delete wiregraph button in configuration tab

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/GwtDevice.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/GwtDevice.java
@@ -47,7 +47,8 @@ public class GwtDevice extends GwtUpdatableEntityModel implements Serializable {
         APP_PROV_V3("PROV-V3"), //
         APP_VPN_CLIENT("VPNCLIENT-V1"), //
         APP_CERTIFICATES("CERT-V1"), //
-        APP_ASSET_V1("ASSET-V1");
+        APP_ASSET_V1("ASSET-V1"),
+        APP_WIRE_V1("WIRE-V1");
 
         private final String appId;
 
